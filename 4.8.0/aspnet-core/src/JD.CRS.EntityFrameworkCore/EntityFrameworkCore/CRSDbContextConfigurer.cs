0992fb07891b2032b4d70af1b9d98fec3bac30eb
xiaoDingc <771801383@qq.com>
2019-11-03 22:22:14 +0800

    增加webapi启动过程中 sql语句输出功能

--- 4.8.0/aspnet-core/src/JD.CRS.EntityFrameworkCore/EntityFrameworkCore/CRSDbContextConfigurer.cs
+++ 4.8.0/aspnet-core/src/JD.CRS.EntityFrameworkCore/EntityFrameworkCore/CRSDbContextConfigurer.cs
@@ -1,18 +1,34 @@
 using System.Data.Common;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace JD.CRS.EntityFrameworkCore
 {
     public static class CRSDbContextConfigurer
     {
+        /// <summary>
+        /// 创建 LoggerFactory 的单一实例/全局实例 控制台记录
+        /// 面ConsoleLoggerProvider的构造函数将在未来的3.0中过时，现在可以不管
+        /// </summary>
+        public static readonly  LoggerFactory MyLoggerFactory=
+            new LoggerFactory(new []
+            {
+                new ConsoleLoggerProvider((categoty,level)=>
+                categoty==DbLoggerCategory.Database.Command.Name
+                && level==LogLevel.Information,true
+                    ),
+            });
         public static void Configure(DbContextOptionsBuilder<CRSDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            builder.UseLoggerFactory(MyLoggerFactory).UseSqlServer(connectionString);
+           // builder.UseSqlServer(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<CRSDbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            builder.UseLoggerFactory(MyLoggerFactory).UseSqlServer(connection);
+            //builder.UseSqlServer(connection);
         }
     }
 }
